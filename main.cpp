443c34db9c0fac8481427028fe42a88a77a67c20
alvarosamudio <alvarosamudio@criptext.com>
2020-05-10 10:46:00 -0500

    add Logo

--- main.cpp
+++ main.cpp
@@ -64,10 +64,10 @@ int main(int argc, char *argv[])
     app.setAttribute(Qt::AA_UseHighDpiPixmaps);
     app.setTheme("light");
     app.setOrganizationName("deepin");
-    app.setApplicationName("dde camera");
+    app.setApplicationName("deepin camera");
     app.setApplicationVersion("1.0");
     app.setProductIcon(QIcon(":/images/logo.svg"));
-    app.setProductName("DDE Camera");
+    app.setProductName("Deepin Camera");
     app.setApplicationDescription("Camera for deepin.");
     //DMainWindow camera;
     //camera.show ();
